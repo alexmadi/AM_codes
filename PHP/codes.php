4ae7cedb57b20b630181811a6167b06e26abc351
Oleksandr Madi <oleksandrmad@gmail.com>
2016-11-28 22:47:58 +0200

    edited sunnyfit config

--- PHP/codes.php
+++ PHP/codes.php
@@ -2,3 +2,7 @@
 <ul class="lang_switcher">
       <?php pll_the_languages(array('show_flags'=>1, 'show_names'=>1,'hide_current'=>1)); ?>
 </ul>
+
+
+// turn off adding extra <p>
+remove_filter('the_content', 'wpautop'); (in functions.php)
