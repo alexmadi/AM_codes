ea2c0fece7dd9dfee8234240d9eaeaee7dc2425e
Oleksandr Madi <oleksandrmad@gmail.com>
2016-12-23 20:35:23 +0200

    added fonts ex

--- PHP/codes.php
+++ PHP/codes.php
@@ -3,6 +3,11 @@
       <?php pll_the_languages(array('show_flags'=>1, 'show_names'=>1,'hide_current'=>1)); ?>
 </ul>
 
+ <?php 
+if (function_exists('pll_the_languages')) {
+	pll_the_languages(array('show_flags'=>1, 'show_names'=>1,'hide_current'=>1));
+}
+?>
 
 // turn off adding extra <p>
 remove_filter('the_content', 'wpautop'); (in functions.php)
