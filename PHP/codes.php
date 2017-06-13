992e8ec099f840c371879ce9055317026115c9fb
Oleksandr <oleksandrmad@gmail.com>
2017-06-13 09:33:20 +0300

    added host info zakmebli

--- PHP/codes.php
+++ PHP/codes.php
@@ -11,3 +11,11 @@ if (function_exists('pll_the_languages')) {
 
 // turn off adding extra <p>
 remove_filter('the_content', 'wpautop'); (in functions.php)
+
+add_filter('pre_get_document_title', 'change_404_title');
+function change_404_title($title) {
+    if (is_404()) {
+        return 'My Custom Title';
+    }
+    return $title;
+}
